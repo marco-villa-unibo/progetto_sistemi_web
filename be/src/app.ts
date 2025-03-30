a52919b3149478efad4309589eeb6c0ef14489e0
mvilla <mvilla@imolainformatica.it>
2025-03-30 21:39:16 +0200

    app boiler

---
+++ be/src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+// BODY-PARSE
+app.use(express.json());
+
+// CORS - REST-API setting
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.listen(8080);
