62249ce512c6668448d27cf8d863a79141256d32
marco-villa-unibo <marco.villa13@studio.unibo.it>
2025-03-30 23:06:03 +0200

    add db connection

---
+++ be/src/util/db.ts
@@ -0,0 +1,12 @@
+import mysql from 'mysql2';
+import { DB_HOST, DB_NAME, DB_USER, DB_PASS } from './config';
+
+const poolConnection = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASS,
+});
+
+const pool = poolConnection.promise();
+export { pool };
