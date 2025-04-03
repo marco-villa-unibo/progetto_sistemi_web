a69b85c36b3a97c2122423453695a1e574445029
marco-villa-unibo <marco.villa13@studio.unibo.it>
2025-04-03 11:48:53 +0200

    fix

--- be/src/utils/db.ts
+++ be/src/utils/db.ts
@@ -1,12 +1,9 @@
 import mysql from 'mysql2';
 import { DB_HOST, DB_NAME, DB_USER, DB_PASS } from '../configs/envConfig';
 
-const poolConnection = mysql.createPool({
+export const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   database: DB_NAME,
   password: DB_PASS,
 });
-
-const pool = poolConnection.promise();
-export { pool };
