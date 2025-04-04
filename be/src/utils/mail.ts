9b37b47c4a8f21e9e5cc75165a75352bd7ead909
marco-villa-unibo <marco.villa13@studio.unibo.it>
2025-04-04 14:37:38 +0200

    add mail service set-up

---
+++ be/src/utils/mail.ts
@@ -0,0 +1,26 @@
+import nodemailer, { SentMessageInfo } from 'nodemailer';
+import { SG_API_KEY } from '../configs/envConfig';
+
+// NOTE: mail provider settings
+// const transporter = nodemailer.createTransport(
+//   snt({
+//     auth: {
+//       api_key: SG_API_KEY,
+//     },
+//   })
+// );
+
+const transporter = nodemailer.createTransport();
+// NOTE: fattorizzata per poter usare in tutta l'applicazione (es. Sign-In)
+export const mailingService = (
+  to: string,
+  subject: string,
+  htmlBody: string
+): Promise<SentMessageInfo> => {
+  return transporter.sendMail({
+    to: to,
+    from: 'shop@sistemi.web',
+    subject: subject,
+    html: htmlBody,
+  });
+};
