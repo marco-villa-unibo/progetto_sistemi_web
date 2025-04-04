4e0981ece1c7d3397df1ef5ba7e85abe0763ac41
marco-villa-unibo <marco.villa13@studio.unibo.it>
2025-04-04 16:26:30 +0200

    fix

--- be/src/middlewares/error.ts
+++ be/src/middlewares/error.ts
@@ -7,18 +7,10 @@ export function errorHandler(
   req: Request,
   res: Response<ErrorDto>,
   next: NextFunction
-): void {
+) {
   console.error(err.stack);
 
-  // Controlla se l'errore è di tipo HttpError, se sì usa il codice di stato
-  if (err instanceof HttpError) {
-    console.log(
-      `Errore tipo HttpError, stato: ${err.status}, messaggio: ${err.message}`
-    );
-    res.status(err.status).send({ message: err.message });
-  } else {
-    // Per gli altri errori non personalizzati
-    console.log('Errore sconosciuto: ', err);
-    res.status(500).send({ message: 'Internal Server Error' });
-  }
+  res
+    .status(err.status || 500)
+    .send({ message: err.message || 'Internal Server Error' });
 }
