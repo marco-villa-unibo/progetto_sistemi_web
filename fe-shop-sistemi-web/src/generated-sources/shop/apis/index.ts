4a521f90df8342418ffe564b94eac8cf13323ef4
marco-villa-unibo <marco.villa13@studio.unibo.it>
2025-05-07 22:33:09 +0200

    add generated resources

---
+++ fe-shop-sistemi-web/src/generated-sources/shop/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './CartApi';
+export * from './CartItemsApi';
+export * from './HealthApi';
+export * from './OrdersApi';
+export * from './ProductApi';
+export * from './UserApi';
---
+++ fe-shop-sistemi-web/src/generated-sources/shop/models/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddItemToCartRequest';
+export * from './CartItem';
+export * from './CartWithItems';
+export * from './Category';
+export * from './CreateOrderRequest';
+export * from './ErrorModel';
+export * from './HealthStatus';
+export * from './Order';
+export * from './OrderItem';
+export * from './OrderStatus';
+export * from './OrderWithItems';
+export * from './Product';
+export * from './ProductOutput';
+export * from './ProductPrice';
+export * from './ProductQuantity';
+export * from './UpdateCartItemQuantityRequest';
+export * from './UpdateOrderStatusRequest';
+export * from './User';
+export * from './UserInput';
+export * from './UserLogin';
+export * from './UserOutput';
+export * from './UserRegister';
+export * from './UserRole';
